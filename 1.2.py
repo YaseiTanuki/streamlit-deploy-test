db90342001b498abbb706fe931223ffbd716b201
Tanuki <Tanuki1707@gmail.com>
2024-06-07 08:57:26 +0700

    Add files via upload

---
+++ 1.2.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title('This is a next level SEO app')
+st.write('Make your ideal real')
+st.write('What is your seed keyword?')
+title = st.text_input('', value='', label_visibility='hidden')
